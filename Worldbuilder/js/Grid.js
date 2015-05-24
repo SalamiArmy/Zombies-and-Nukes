2bea4cbc29a8745c9941039113a1fa056454e20a
SalamiArmy <ultrakiff@gmail.com>
2015-05-24 09:57:02 +0200

    click hex to edit in worldbuilder

--- Worldbuilder/js/Grid.js
+++ Worldbuilder/js/Grid.js
@@ -6,7 +6,7 @@ HT.Grid = function(/*double*/ width, /*double*/ height) {
 	
 	this.Hexes = [];
 	//setup a dictionary for use later for assigning the X or Y CoOrd (depending on Orientation)
-	var HexagonsByXOrYCoOrd = {}; //Dictionary<int, List<Hexagon>>
+	var HexagonsByXOrYCoOrd = {};
 
 	var row = 0;
 	var y = 0.0;
@@ -17,17 +17,14 @@ HT.Grid = function(/*double*/ width, /*double*/ height) {
 		var offset = 0.0;
 		if (row % 2 == 1)
 		{
-			if(HT.Hexagon.Static.ORIENTATION == HT.Hexagon.Orientation.Normal)
-				offset = (HT.Hexagon.Static.WIDTH - HT.Hexagon.Static.SIDE)/2 + HT.Hexagon.Static.SIDE;
-			else
-				offset = HT.Hexagon.Static.WIDTH / 2;
+			offset = HT.Hexagon.Static.WIDTH / 2;
 			col = 1;
 		}
 		
 		var x = offset;
 		while (x + HT.Hexagon.Static.WIDTH <= width)
 		{
-		    var hexId = this.GetHexId(row, col);
+		    var hexId = col+","+row;
 			var h = new HT.Hexagon(hexId, x, y);
 			
 			var pathCoOrd = col;
@@ -41,16 +38,10 @@ HT.Grid = function(/*double*/ width, /*double*/ height) {
 			HexagonsByXOrYCoOrd[pathCoOrd].push(h);
 
 			col+=2;
-			if(HT.Hexagon.Static.ORIENTATION == HT.Hexagon.Orientation.Normal)
-				x += HT.Hexagon.Static.WIDTH + HT.Hexagon.Static.SIDE;
-			else
-				x += HT.Hexagon.Static.WIDTH;
+			x += HT.Hexagon.Static.WIDTH;
 		}
 		row++;
-		if(HT.Hexagon.Static.ORIENTATION == HT.Hexagon.Orientation.Normal)
-			y += HT.Hexagon.Static.HEIGHT / 2;
-		else
-			y += (HT.Hexagon.Static.HEIGHT - HT.Hexagon.Static.SIDE)/2 + HT.Hexagon.Static.SIDE;
+		y += (HT.Hexagon.Static.HEIGHT - HT.Hexagon.Static.SIDE)/2 + HT.Hexagon.Static.SIDE;
 	}
 
 	//finally go through our list of hexagons by their x co-ordinate to assign the y co-ordinate
@@ -67,20 +58,6 @@ HT.Grid = function(/*double*/ width, /*double*/ height) {
 				h.PathCoOrdX = coOrd2++;
 		}
 	}
-};
-
-HT.Grid.Static = {Letters:'ABCDEFGHIJKLMNOPQRSTUVWXYZ'};
-
-HT.Grid.prototype.GetHexId = function(row, col) {
-	var letterIndex = row;
-	var letters = "";
-	while(letterIndex > 25)
-	{
-		letters = HT.Grid.Static.Letters[letterIndex%26] + letters;
-		letterIndex -= 26;
-	}
-		
-	return HT.Grid.Static.Letters[letterIndex] + letters + (col + 1);
 };
 
 /**
