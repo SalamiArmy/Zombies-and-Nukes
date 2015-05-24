1d385df38ee48e0ef70b882e8824613bedffc4d3
SalamiArmy <ultrakiff@gmail.com>
2015-05-24 20:04:31 +0200

    Build was still gonna be wrong

--- Worldbuilder/js/MouseHandler.js
+++ Worldbuilder/js/MouseHandler.js
@@ -7,8 +7,9 @@
 }
 
 function SelectNewHex(canvas, message) {
+    //document.getElementById("HexData").value = HexData;
+    document.getElementById("SessionCoord").value = message;
     //xmlhttp.open("GET", "http://localhost:3142/services/Zombies%20and%20Nukes/World%20Builder/World%20Builder%20Core/Update%20Session.json?SessionName=" + document.getElementById("SessionName").innerText + "&SessionCoord=" + document.getElementById("SessionCoord").value + "&OffSet=" + document.getElementById("OffSet").value);
-    document.getElementById("ID").value = message;
 }
 
 function AddMouseHandler(canvas, grid) {
