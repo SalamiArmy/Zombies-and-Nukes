b72fbe1754987edbcbf23c7ed973eed80be1387d
Salami Army <ultrakiff@gmail.com>
2017-09-09 05:59:03 +0200

    fix encrypt decrypt scripts that were hibly broken in places

--- EncryptNarratives.bat
+++ EncryptNarratives.bat
@@ -1,9 +1,14 @@
 if exist "%~dp0PublicNarrationKey.ini" (
-	for /f "delims=|" %%f in ('dir /b /s %~dp0Narration\*.txt') do "%~dp0openssl-0.9.8h-1-bin\openssl.exe" aes-256-cbc -salt -a -e -in "%%f" -out "%%f.encrypted" -pass file:%~dp0PublicNarrationKey.ini
+	if exist "%~dp0Narration\*.txt" (
+		for /f "delims=|" %%f in ('dir /b /s %~dp0Narration\*.txt') do "%~dp0openssl-0.9.8h-1-bin\openssl.exe" aes-256-cbc -salt -a -e -in "%%f" -out "%%f.encrypted" -pass file:%~dp0PublicNarrationKey.ini
+	) else (
+		echo "%~dp0Narration\*.txt" file missing.
+		pause
+	)
 ) else (
 	echo "%~dp0PublicNarrationKey.ini" file missing. As spoiler protection for players the narrations require a key to unlock by a GM.
 	pause
 )
 
-if exist "%~dp0NormalGMNarrationKey.ini" for /f "delims=|" %%f in ('dir /b /s %~dp0Narration\Normal\*.encrypted') do "%~dp0openssl-0.9.8h-1-bin\openssl.exe" aes-256-cbc -salt -a -e -in "%%f" -out "%%f.encrypted" -pass file:"%~dp0NormalGMNarrationKey.ini"
-if exist "%~dp0FinaleNarrationKey.ini" for /f "delims=|" %%f in ('dir /b /s %~dp0Narration\Finale\*.encrypted') do "%~dp0openssl-0.9.8h-1-bin\openssl.exe" aes-256-cbc -salt -a -e -in "%%f" -out "%%f.encrypted" -pass file:"%~dp0FinaleNarrationKey.ini"
+if exist "%~dp0NormalGMNarrationKey.ini" if exist "%~dp0Narration\Normal\*.txt" for /f "delims=|" %%f in ('dir /b /s %~dp0Narration\Normal\*.txt') do "%~dp0openssl-0.9.8h-1-bin\openssl.exe" aes-256-cbc -salt -a -e -in "%%f" -out "%%f.encrypted" -pass file:"%~dp0NormalGMNarrationKey.ini"
+if exist "%~dp0FinaleNarrationKey.ini" if exist "%~dp0Narration\Finale\*.txt" for /f "delims=|" %%f in ('dir /b /s %~dp0Narration\Finale\*.txt') do "%~dp0openssl-0.9.8h-1-bin\openssl.exe" aes-256-cbc -salt -a -e -in "%%f" -out "%%f.encrypted" -pass file:"%~dp0FinaleNarrationKey.ini"
